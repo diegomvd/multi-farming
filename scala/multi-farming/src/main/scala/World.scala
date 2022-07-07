6635232abece2223578c0a130666a188bda679a1
Diego <diego.bengocheapaz@pm.me>
2022-07-07 11:05:18 +0200

    Progress in defining the world and sim steps

--- scala/multi-farming/src/main/scala/World.scala
+++ scala/multi-farming/src/main/scala/World.scala
@@ -2,9 +2,9 @@ class World(eco: Graph[EcoUnit,Long],
             pln: Graph[PlanningUnit,Long],
             mng: Graph[ManagementUnit,Long],
             pop: Double,
-            params: Parameters){
+            args: Parameters){
 
-  private var world: World = buildWorld()
+  private var world: World = apply(args)
 
   def update(): World = {
     // the biophysical landscape is updated with the changes. the other landscapes
@@ -14,45 +14,32 @@ class World(eco: Graph[EcoUnit,Long],
       case ("recovery",_) => {
         val new_unit = EcoUnit() // copy the unit and copy the landscape
         val new_biophy_layer
-        val new_planning_layer
-        val new_management_layer
         World(new_biophy_layer,new_planning_layer,new_management_layer,this.population_layer,this.params)
       }
       case ("degradation",_) => {
         val new_unit = EcoUnit() // copy the unit and copy the landscape
         val new_biophy_layer
-        val new_planning_layer
-        val new_management_layer
         World(new_biophy_layer,new_planning_layer,new_management_layer,this.population_layer,this.params)
       }
       case ("fertility_loss",_) => {
         val new_unit = EcoUnit() // copy the unit and copy the landscape
         val new_biophy_layer
-        val new_planning_layer
-        val new_management_layer
         World(new_biophy_layer,new_planning_layer,new_management_layer,this.population_layer,this.params)
       }
       case ("low_intensity",_) => {
         val new_unit = EcoUnit() // copy the unit and copy the landscape
         val new_biophy_layer
-        val new_planning_layer
-        val new_management_layer
         World(new_biophy_layer,new_planning_layer,new_management_layer,this.population_layer,this.params)
       }
       case ("high_intensity",_) => {
         val new_unit = EcoUnit() // copy the unit and copy the landscape
         val new_biophy_layer
-        val new_planning_layer
-        val new_management_layer
         World(new_biophy_layer,new_planning_layer,new_management_layer,this.population_layer,this.params)
       }
       case ("birth",_) =>
       case ("death",_) =>
 
     }
-
-    landscape = landscape.update
-    landscape
   }
 
   def hasNext(): Boolean = {
@@ -109,7 +96,95 @@ object World{
     World(eco,pln,mng,pop)
   }
 
-  def nextEvent(): String = {}
+  def updated(pop: (Double,Double),
+              rec: (VertexRDD[Double],Double),
+              deg: (VertexRDD[Double],Double),
+              flo: (VertexRDD[Double],Double),
+              mng: Double,
+              eco: Graph[EcoUnit,Long],
+              pln: Graph[PlnUnit,Long],
+              mng: Graph[MngUnit,Long]): World = {
 
+    val tpop: Double = pop._1 + pop._2
+    val tspont: Double = rec._2 + deg._2 + flo._2
+
+    val x_rand: Double = rnd.nextDouble(tpop+tspont+mng)
+
+    selectEventGeneralType(x_rand, spont, mng, pop) match {
+      case "Population" => {
+        selectBirthOrDeath(x_rand,pop) match {
+          case "Birth" => World(eco,pln,mng,pop+1,args)
+          case "Death" => World(eco,pln,mng,pop-1,args)
+        }
+      }
+      case "Spontaneous" => {
+        selectSpontaneous(x_rand,(rec._2,deg._2,flo._2)) match {
+          case "Recovery" => {
+            val vid = S3Utils.selectVId(x_rand,rec._1)
+            val new_eco = EcoLandscape.updated(vid,"Natural")
+          }
+          case "Degradation" => {
+            val vid = S3Utils.selectVId(x_rand,deg._1)
+            val new_eco = EcoLandscape.updated(vid,"Degraded")
+          }
+          case "FertilityLoss" => {
+            val vid = S3Utils.selectVId(x_rand,flo._1)
+            val new_eco = EcoLandscape.updated(vid,"Degraded")
+          }
+        }
+        World(new_eco,pln,mng,pop,args)
+      }
+      case "Management"
+
+    }
+
+  }
+
+  /**
+  * @param x_rand is the random number thrown to sample the distributions
+  * @param spont is the total spontaneous propensity + the population one
+  * @param mng is the total management propensity + spontaneous + population
+  * @param pop is the total population propensity
+  * @return a string identifying the general type of event
+  */
+  def selectEventGeneralType(x_rand: Double,
+                             spont: Double,
+                             mng: Double,
+                             pop: Double): String = {
+    x_rand match {
+      case x if x < pop => "Population"
+      case x if x < spont => "Spontaneous"
+      case x if x < mng => "Management"
+      case other => println(s"Cannot select a transition, propensity upper bound in the selector is larger than expected.")
+    }
+  }
+
+  /**
+  * @param x_rand is the random number thrown to sample the distributions
+  * @param prop contains the birth and death propensities in field 1 and 2 respectively
+  * @return a string with the type of human population event
+  */
+  def selectBirthOrDeath(x_rand: Double,
+                         prop: (Double, Double)): String = {
+    x_rand match {
+      case x if x < prop._1 => "Birth"
+      case x if x < prop._2 => "Death"
+      case other => println(s"selectBirthOrDeath: Cannot select a transition, propensity upper bound in the selector is larger than expected.")
+    }
+  }
+
+  /**
+  * @param x_rand is the random number thrown to sample the distributions
+  * @param prop contains the recovery, degradation and fertility loss propensities in field 1,2 and 3 respectively
+  * @return a string with the type of spontaneous land cover transition
+  */
+  def selectSpontaneous(x_rand: Double,
+                        prop: (Double,Double,Double)): String = {
+    x_rand match {
+      case x if x < prop._1 => "Recovery"
+      case x if x < prop._2 => "Degradation"
+      case x if x < prop._3 => "FertilityLoss"
+      case other => println(s"selectSpontaneous: Cannot select a transition, propensity upper bound in the selector is larger than expected.")
+  }
 }
 // val parameters = InputOutput.parseArgs(args)
