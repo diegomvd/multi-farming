9892217f43f1edcacf3d6dc81974ed14d639096d
Diego <diego.bengocheapaz@pm.me>
2022-07-23 13:32:40 +0200

    re arrangement of utils in voronoi and stochastic simulation

--- scala/multi-farming/src/main/scala/VoronoiTesselation.scala
+++ scala/multi-farming/src/main/scala/VoronoiTesselation.scala
@@ -4,6 +4,13 @@ a number of seeds, or different voronoi polygons, and a base landscape with a no
 of neighborhood to tesselate over it. The landscape is provided as a Graph, since
 by construction it contains the neighborhood information.
 */
+
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.Edge
+import org.apache.spark.graphx.Graph
+
 object VoronoiTesselation{
 
   /**
@@ -119,22 +126,4 @@ object VoronoiTesselation{
     val edges: RDD[Edge[Long]] = newEdges(vertices,base)
     Graph(vertices,edges)
   }
-}
-
-
-object S3Utils{ // utility functions for spatial stochastic simulations
-
-  // could be transformed to ParMap[Int,Double] to make it more general
-  def positionSelector(prob: ParMap[ModuloCoord, Double]) = ModuloCoord {
-    // one liner to get the position by ordering the map into a vector map
-    val x_rand = rnd.nextDouble(1.0)
-    VectorMap(prob.toSeq.sortBy(_._1.m):_*).scanLeft[(ModuloCoord,Double)]((ModuloCoord(-1),0.0)){case ((_,acc),(pos,x)) => (pos, acc + x)}.find((_,x) => x_rand <= x)._1
-  }
-
-
-  def selectPosition(propensity: ParMap[ModuloCoord,Double], add: Double, x_rand: Double) = ModuloCoord {
-    S3Utils.positionSelector(propensity.map( _._1 -> _._2 + add ).toMap.par)
-  }
-
-
 }
